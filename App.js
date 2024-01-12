04c7f424d3b629841f58a4ccb4f0ae28f82012f9
Pradnya136 <pradnyasonawane133@gmail.com>
2024-01-12 14:47:31 +0530

    completed 3rd assignment

---
+++ App.js
@@ -0,0 +1,55 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// react element
+const heading1 = React.createElement("h1"," " ,"this is h1")
+const heading2 = React.createElement("h2"," " ,"this is h2")
+const heading3 = React.createElement("h3"," " ,"this is h3")
+const header = React.createElement("div", {id:"head"}, [heading1,heading2,heading3])
+
+
+// JSX Element
+const jsxelement = 
+                (<div className="title">
+                    <h1>h1 jsx element</h1>
+                    <h2>h1 jsx element</h2>
+                    <h3>h1 jsx element</h3>
+                </div>)
+
+
+// functional component
+const JsxMainHeader = () => <h1>This is the main header of JSX</h1>
+
+
+const JsxHeader = () => (
+
+    <div className="title">
+        <JsxMainHeader></JsxMainHeader>
+        <JsxMainHeader/>
+        {<JsxMainHeader/>}
+        <h1>This jsx h1</h1>
+        <h2>This jsx h2</h2>
+        <h3>This jsx h3</h3>
+    </div>
+)
+
+// ---------------------------------
+
+const Logo = ()=> <div className="logo">Logo</div>
+
+const SearchBar = () => <div className="searchbar">Search Bar</div>
+
+const UserIcon = () => <div className="icon">Icon</div>
+
+const Headerr = () => (
+    <div id="wrapper">
+        <Logo/>
+        <SearchBar/> 
+        <UserIcon/>
+    </div>
+)
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<Headerr/>)
---
+++ readme.md
@@ -0,0 +1,18 @@
+create files html, css, app.js
+write code in html - create root div 
+create css file add in html
+we used cdn to inject react first then removed those cdn links
+did npm init to create package.json file
+installed react n react dom - created react elements using react.createElement and import react using jsx n fucntional component
+installed parcel bundler to start project by npx parcel index.html
+edited scripts added start n build - so now we use npm start n npm run build 
+installed browerslist
+
+
+-----------------------------
+to create package.json - npm init 
+
+
+--------------------------------
+to initialize git - git init
+den 
