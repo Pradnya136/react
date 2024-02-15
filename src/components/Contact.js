30a269b9b0fe352e2cf28eb9b4a5e03d4b25eea1
Pradnya136 <pradnyasonawane133@gmail.com>
2024-02-15 23:16:01 +0530

    nothing

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,13 +5,13 @@ const Contact = ()=>{
         <div className="justify-center flex flex-col items-center">
             <h1 className="p-4 m-2 font-bold text-3xl">Contact us !</h1>
        
-            <form>
+            {/* <form>
                 <label>Name:</label>
                 <input/>
                 <label>Address:</label>
                 <input/>
                 <button>submit</button>
-            </form>
+            </form> */}
       
         </div>
     )
